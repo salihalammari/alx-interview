015b3e49e58a5e86d8ba4506639c888b5fc81ff6
salihalammari <slammari@student.1337.ma>
2024-06-25 13:50:16 -0700

    add pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+create 0-pascal_triangle.py
+"""
+
+def pascal_triangle(n):
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i -1]
+
+        for j in range(len(prev_row) - 1):
+            row.append(prev_row[j] + prev_row[j + 1])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,4 @@
+Curriculum
+# Short Specializations
+
+0x00. Pascal's Triangle
