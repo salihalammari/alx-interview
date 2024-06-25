c9f928e88d0d62ca07c63ca577fb7f76eb0eca78
salihalammari <slammari@student.1337.ma>
2024-06-25 14:05:38 -0700

    According to PEP8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,10 +2,8 @@
 """
 create 0-pascal_triangle.py
 """
-
 def pascal_triangle(n):
     """
-
     Returns a list of integers
     representing the Pascal Triangle of n
     returns empty list if n <= 0
@@ -16,7 +14,7 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         row = [1]
-        prev_row = triangle[i -1]
+        prev_row = triangle[i - 1]
 
         for j in range(len(prev_row) - 1):
             row.append(prev_row[j] + prev_row[j + 1])
