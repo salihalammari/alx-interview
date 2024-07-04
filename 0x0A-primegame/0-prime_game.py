a9c8146686cfb8ddc621f85275f5145cbd060509
salihalammari <slammari@student.1337.ma>
2024-07-03 19:03:12 -0700

    fix all erroe

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,46 +2,42 @@
 """Module for Prime Game"""
 
 
-def sieve_of_eratosthenes(max_num):
-    """ Return a list where the index represents a potential prime number."""
-    sieve = [True] * (max_num + 1)
-    sieve[0] = sieve[1] = False  # 0 and 1 are not prime numbers
-    p = 2
-    while (p * p <= max_num):
-        if (sieve[p] == True):
-            # Marking the multiples of p as non-prime
-            for i in range(p * p, max_num + 1, p):
-                sieve[i] = False
-        p += 1
-    return sieve
-
-
-
 def isWinner(x, nums):
-
-    if x <= 0 or nums is None or x != len(nums):
+    # Check for invalid input
+    if x <= 0 or nums is None:
         return None
-
-    max_n = max(nums)
-    primes_sieve = sieve_of_eratosthenes(max_n)
-
-    ben_wins = 0
-    maria_wins = 0
-
-    for n in nums:
-        if n <= 1:
-            continue
-
-        primes_count = sum(primes_sieve[:n + 1])
-
-        if primes_count % 2 == 0:
-            ben_wins += 1
+    if x != len(nums):
+        return None
+    # Initialize scores and array of possible prime numbers
+    ben = 0
+    maria = 0
+    # Create a list 'a' of length sorted(nums)[-1] + 1 with all elements
+    # initialized to 1
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    # The first two elements of the list, a[0] and a[1], are set to 0
+    # because 0 and 1 are not prime numbers
+    a[0], a[1] = 0, 0
+    # Use Sieve of Eratosthenes algorithm to generate array of prime numbers
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+    # Play each round of the game
+    for i in nums:
+        # If the sum of prime numbers in the set is even, Ben wins
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
         else:
-            maria_wins += 1
-
-    if ben_wins > maria_wins:
+            maria += 1
+    # Determine the winner of the game
+    if ben > maria:
         return "Ben"
-    elif maria_wins > ben_wins:
+    if maria > ben:
         return "Maria"
-    else:
-        return None
+    return None
+
+
+def rm_multiples(ls, x):
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
