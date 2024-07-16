ea7771c062c3d148dff3a9b48aaa04c672b8a54d
salihalammari <slammari@student.1337.ma>
2024-07-16 13:37:15 -0700

    correct error norme

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,20 +2,18 @@
 """2D matrix rotation module.
 """
 
+
 def rotate_2d_matrix(matrix):
     """Rotates an m by n 2D matrix in place.
     """
     if not isinstance(matrix, list):
         return
-    
     rows = len(matrix)
     if rows == 0:
         return
-    
     cols = len(matrix[0])
     if not all(isinstance(row, list) and len(row) == cols for row in matrix):
         return
-    
     # Perform matrix rotation
     for layer in range(rows // 2):
         first = layer
@@ -23,17 +21,12 @@ def rotate_2d_matrix(matrix):
         for i in range(first, last):
             offset = i - first
             top = matrix[first][i]
-            
             # left -> top
             matrix[first][i] = matrix[last - offset][first]
-            
             # bottom -> left
             matrix[last - offset][first] = matrix[last][last - offset]
-            
             # right -> bottom
             matrix[last][last - offset] = matrix[i][last]
-            
             # top -> right
             matrix[i][last] = top
-    
     return matrix
